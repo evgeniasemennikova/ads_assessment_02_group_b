32bf69e1e04c70489b1e2b332a38333066b1de50
evgeniasemennikova <evgeniasemennikova1@gmail.com>
2024-04-30 11:11:52 +0200

    assessment2

--- main.py
+++ main.py
@@ -3,13 +3,31 @@ Multiplication Table
 """
 # Provide your solution here
 
+def print_multiplication_table(a: int, b: int):
+        result = a * b
+        print("a*b =", result)
+        return result
+if a or b < 0:
+    print("Number and rows cannot be less then 1")
+
+
+
+
 """
 Palindromes
 """
 # Provide your solution here
+def is_palindrome():
+    print()
+
+
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
+
+print_multiplication_table(2,5)
 
 
